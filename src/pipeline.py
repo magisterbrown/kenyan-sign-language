d1a9e3157b16c03b3ce16126502e811b85d52ef7
Irmak Celebi <irmakcelebi99@gmail.com>
2021-11-27 12:26:03 +0100

    TODO add resize function after making model

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,7 +1,16 @@
 from preprocessing.dataloader import make_dataloader_from_npy
 from config.paths import npy_path
+from preprocessing.convert_to_npy import from_image_to_numpy
 
-# change the npy path according to which resized image data you would like to ue
+'''
+This script is the overall pipeline for building dataloaders and training the models
+'''
+
+# If you want to resize and resave the images uncomment this part
+# make resize field True and add a new path to save the npy files
+# from_image_to_numpy()
+
+# Change the npy path according to which resized image data you would like to ue
 dl = make_dataloader_from_npy(npy_path)
 
 
--- src/preprocessing/convert_to_npy.py
+++ src/preprocessing/convert_to_npy.py
@@ -6,15 +6,22 @@ import numpy as np
 from config.paths import image_path, npy_path
 
 
-def from_image_to_numpy():
+def from_image_to_numpy(resize=False, resized_path=None):
     dirs = os.listdir(str(image_path))
     dirs.sort()
+    path = npy_path
+
+    if resize:
+        # TODO: add resize function
+        print('resize')
+        path = resized_path
 
     for item in dirs:
         im = Image.open(os.path.join(image_path, item)).convert("RGB")
-        if not (os.path.exists(npy_path)):
-            os.mkdir(npy_path)
-        data_path = os.path.join(npy_path, item[:-4] + '.npy')
+        if not (os.path.exists(path)):
+            os.mkdir(path)
+
+        data_path = os.path.join(path, item[:-4] + '.npy')
         image_array = np.array(im)
         np.save(data_path, image_array)
 
